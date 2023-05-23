9a03c0b8c0cc473c7ff8b30ecb357f8179d05334
Centli <cgarcesbuendia@appen.com>
2023-05-23 16:57:14 -0600

    ADD messages to every assert method

--- src/test/java/tests/CheckUtilTestNgTest.java
+++ src/test/java/tests/CheckUtilTestNgTest.java
@@ -4,7 +4,6 @@ import java.security.PublicKey;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
-
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import org.testng.Assert;
@@ -14,13 +13,13 @@ import pages.CheckUtilTestNg;
 
 public class CheckUtilTestNgTest {
 
-    String driverPath = "/src/main/resources/drivers/chromedriver";
+    String driverPath = "src/main/resources/drivers/chromedriver";
     static String pirateUrl = "https://codility-frontend-prod.s3.amazonaws.com/media/task_static/qa_search/6f03f4361b080eeb747193aadd94cd2b/static/attachments/reference_page.html";
     WebDriver driver;
 
     CheckUtilTestNg objCheckUtilTestNg;
 
-    @BeforeClass
+    @BeforeClass(alwaysRun = true)
 
     public void setup(){
         System.setProperty("webdriver.chrome.driver", driverPath);
@@ -39,53 +38,53 @@ public class CheckUtilTestNgTest {
     /**
      * This case will check if query input and submit button are on the main screen of the application
      */
-    @Test(priority = 0)
+    @Test()
 
     public void query_input_and_submit_button_are_on_the_main_screen(){
         //Create CheckUtilTestNgTest object
 
         objCheckUtilTestNg = new CheckUtilTestNg(driver);
 
-        Assert.assertTrue(objCheckUtilTestNg.getInputId());
-        Assert.assertTrue(objCheckUtilTestNg.getSubmitButtonId());
+        Assert.assertTrue(objCheckUtilTestNg.getInputId(),"Verify input text box is on main screen of the application");
+        Assert.assertTrue(objCheckUtilTestNg.getSubmitButtonId(), "Verify submit button is on main screen of the application");
     }
 
-    @Test(priority = 0)
+    @Test()
 
     public void check_if_searching_with_empty_query_is_forbidden(){
         //Create CheckUtilTestNgTest object
 
         objCheckUtilTestNg = new CheckUtilTestNg(driver);
 
-        Assert.assertTrue(objCheckUtilTestNg.setEmptyQuery());
+        Assert.assertTrue(objCheckUtilTestNg.setEmptyQuery(), "Verify if searching with empty query is forbidden");
     }
 
-    @Test(priority = 0)
+    @Test()
 
     public void check_if_searching_with_isla_query_returns_at_least_one_island(){
         //Create CheckUtilTestNgTest object
 
         objCheckUtilTestNg = new CheckUtilTestNg(driver);
 
-        Assert.assertTrue(objCheckUtilTestNg.setIslaQuery());
+        Assert.assertTrue(objCheckUtilTestNg.setIslaQuery(), "Verify if 'island' query is returned at least one <li> result element");
     }
 
-    @Test(priority = 0)
+    @Test()
 
     public void check_if_searching_with_castle_query_returns_no_results_feedback(){
         //Create CheckUtilTestNgTest object
 
         objCheckUtilTestNg = new CheckUtilTestNg(driver);
 
-        Assert.assertTrue(objCheckUtilTestNg.setCastleQuery());
+        Assert.assertTrue(objCheckUtilTestNg.setCastleQuery(), "Verify if user gets feedback if there are no results");
     }
 
-    @Test(priority = 0)
+    @Test()
 
     public void check_if_results_match_the_query(){
         objCheckUtilTestNg = new CheckUtilTestNg(driver);
 
-        Assert.assertTrue(objCheckUtilTestNg.setPortQuery());
+        Assert.assertTrue(objCheckUtilTestNg.setPortQuery(), "Verify if querying for port should return only one result, Port Royal");
     }
 
     @AfterMethod
