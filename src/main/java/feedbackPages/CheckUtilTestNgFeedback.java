f396757beb7460034f9e86994fa5d13d91945b7f
Centli <cgarcesbuendia@appen.com>
2023-05-29 13:13:19 -0600

    CREATE feedbackPages package in order to improve pages package

---
+++ src/main/java/feedbackPages/CheckUtilTestNgFeedback.java
@@ -0,0 +1,88 @@
+package feedbackPages;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class CheckUtilTestNgFeedback {
+    // Variables
+    WebDriver driver;
+
+    String emptyQuery = "";
+
+    String islaQuery = "isla";
+
+    String liElement = "li";
+
+    String castleQuery = "castle";
+
+    String portQuery = "port";
+
+    By queryInputId = By.cssSelector("#search-input");
+    By submitButton = By.cssSelector("#search-button");
+    By ErrorEmptyQueryDivID = By.cssSelector("#error-empty-query");
+
+    By NoResultsDivID = By.cssSelector("#error-no-results");
+
+    public CheckUtilTestNgFeedback(WebDriver driver){
+        this.driver = driver;
+    }
+
+    public Boolean getInputId(){
+        return driver.findElement(queryInputId).isDisplayed();
+    }
+
+    public Boolean getSubmitButtonId(){
+        return driver.findElement(submitButton).isDisplayed();
+    }
+
+    public Boolean getErrorEmptyQueryDiv(){
+        return driver.findElement(ErrorEmptyQueryDivID).isDisplayed();
+    }
+
+    public Boolean setEmptyQuery(){
+        driver.findElement(queryInputId).sendKeys(emptyQuery);
+        WebElement searchButton = driver.findElement(submitButton);
+        searchButton.click();
+
+        return getErrorEmptyQueryDiv();
+    }
+
+    public Boolean GetAtLeastOneIslandDiv(){
+        return driver.findElements(By.tagName(liElement)).size() > 0;
+    }
+
+    public Boolean setIslaQuery(){
+        driver.findElement(queryInputId).sendKeys(islaQuery);
+        WebElement searchButton = driver.findElement(submitButton);
+        searchButton.click();
+
+        return GetAtLeastOneIslandDiv();
+    }
+
+    public Boolean GetANoResultsDiv(){
+        return driver.findElement(NoResultsDivID).isDisplayed();
+    }
+
+    public Boolean setCastleQuery(){
+        driver.findElement(queryInputId).sendKeys(castleQuery);
+        WebElement searchButton = driver.findElement(submitButton);
+        searchButton.click();
+
+        return GetANoResultsDiv();
+    }
+
+    public Boolean GetOnlyOneLiElement(){
+        return driver.findElements(By.tagName(liElement)).size() == 1;
+    }
+
+    public Boolean setPortQuery(){
+        driver.findElement(queryInputId).sendKeys(portQuery);
+        WebElement searchButton = driver.findElement(submitButton);
+        searchButton.click();
+
+        return GetOnlyOneLiElement();
+    }
+
+}
---
+++ src/test/java/tests/CheckUtilTestNgFeedbackTest.java
@@ -0,0 +1,100 @@
+package tests;
+import feedbackPages.CheckUtilTestNgFeedback;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+import feedbackPages.CheckUtilTestNgFeedback;
+
+import java.util.concurrent.TimeUnit;
+
+public class CheckUtilTestNgFeedbackTest {
+
+    WebDriver driver;
+
+    CheckUtilTestNgFeedback objCheckUtilTestNgFeedback;
+
+    private final static String DRIVER_PATH = "/Users/centli.garcesencora.com/Documents/encora/pirate_task/piratesProject/src/main/resources/drivers/chromedriver";
+
+    private final static String BASE_URL = "https://codility-frontend-prod.s3.amazonaws.com/";
+
+    private final static String PATH = "media/task_static/qa_search/6f03f4361b080eeb747193aadd94cd2b/static/attachments/reference_page.html";
+
+    @BeforeClass(alwaysRun = true)
+
+    public void setup(){
+        System.setProperty("webdriver.chrome.driver", DRIVER_PATH);
+
+        driver = new ChromeDriver();
+
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+    }
+
+    @BeforeMethod
+
+    public void openURL(){
+
+        driver.get(BASE_URL+PATH);
+    }
+
+    /**
+     * This case will check if query input and submit button are on the main screen of the application
+     */
+    @Test()
+
+    public void query_input_and_submit_button_are_on_the_main_screen(){
+        //Create CheckUtilTestNgTest object
+
+        objCheckUtilTestNgFeedback = new CheckUtilTestNgFeedback(driver);
+
+        Assert.assertTrue(objCheckUtilTestNgFeedback.getInputId(),"Verify input text box is on main screen of the application");
+        Assert.assertTrue(objCheckUtilTestNgFeedback.getSubmitButtonId(), "Verify submit button is on main screen of the application");
+    }
+
+    @Test()
+
+    public void check_if_searching_with_empty_query_is_forbidden(){
+        //Create CheckUtilTestNgTest object
+
+        objCheckUtilTestNgFeedback = new CheckUtilTestNgFeedback(driver);
+
+        Assert.assertTrue(objCheckUtilTestNgFeedback.setEmptyQuery(), "Verify if searching with empty query is forbidden");
+    }
+
+    @Test()
+
+    public void check_if_searching_with_isla_query_returns_at_least_one_island(){
+        //Create CheckUtilTestNgTest object
+
+        objCheckUtilTestNgFeedback = new CheckUtilTestNgFeedback(driver);
+
+        Assert.assertTrue(objCheckUtilTestNgFeedback.setIslaQuery(), "Verify if 'island' query is returned at least one <li> result element");
+    }
+
+    @Test()
+
+    public void check_if_searching_with_castle_query_returns_no_results_feedback(){
+        //Create CheckUtilTestNgTest object
+
+        objCheckUtilTestNgFeedback = new CheckUtilTestNgFeedback(driver);
+
+        Assert.assertTrue(objCheckUtilTestNgFeedback.setCastleQuery(), "Verify if user gets feedback if there are no results");
+    }
+
+    @Test()
+
+    public void check_if_results_match_the_query(){
+        objCheckUtilTestNgFeedback = new CheckUtilTestNgFeedback(driver);
+
+        Assert.assertTrue(objCheckUtilTestNgFeedback.setPortQuery(), "Verify if querying for port should return only one result, Port Royal");
+    }
+
+    @AfterClass
+    public void tearDown() {
+        driver.quit();
+    }
+
+}
